dd14d905792fb06496bfa7aaeef77c02781c9ef3
sshah1 <sshah0@walmartlabs.com>
2015-09-21 19:52:59 -0700

    Adding the View and controller for the switch view

---
+++ Yelp/SwitchCell.swift
@@ -0,0 +1,28 @@
+//
+//  SwitchCell.swift
+//  Yelp
+//
+//  Created by Saurabh Shah on 9/21/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class SwitchCell: UITableViewCell {
+
+    @IBOutlet weak var switchLabel: UILabel!
+    @IBOutlet weak var onSwitch: UISwitch!    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
